8b9e21f09dfa89391b80e8cbe43b33b967a6bd0a
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-18 15:26:26 +0000

    Desktop styling

--- src/PageTwo.tsx
+++ src/PageTwo.tsx
@@ -5,6 +5,7 @@ const PageTwo = () => {
     <div className='pageTwo'>
         <div className='pageTwoText'>
             <h1>Hi</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed dolores beatae molestiae voluptatibus! Sunt, temporibus aspernatur ratione vitae nihil obcaecati nostrum labore, sequi nobis omnis explicabo magnam! Odit, minima aliquam.</p>
         </div>
         <div className='imageWithOverlay'>
             <h2>TEST LLLOAfASD <br /> fisadf</h2>
