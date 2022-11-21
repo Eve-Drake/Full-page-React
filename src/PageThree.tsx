09bcda7da409f3d17be3eef89468e39cfe72b588
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-21 16:56:30 +0000

    Placeholder text for styling experiment added

--- src/PageThree.tsx
+++ src/PageThree.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PageThree = () => {
   return (
     <div className='pageThree'>
-        <h1>AHHHHHHHHHH</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rerum soluta, omnis consequatur incidunt tenetur et molestias laudantium est eaque enim sit pariatur doloribus corporis a nulla repellendus corrupti autem!</h1>
     </div>
   )
 }
